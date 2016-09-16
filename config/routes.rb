50a5a83d71cbbcffd1980d5f94d22f7b001bb880
chi <chi_bui@outlook.com>
2016-09-16 16:18:26 +1000

    add heart like display

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   # performs CRUD for tweets
   resources :tweets do
   member do
-    put "like", to: "tweets#upvote"
-    put "dislike", to: "tweets#downvote"
+    put "like", to: "tweets#vote"
     end
   end
 
   root 'pages#feed'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 
 end
